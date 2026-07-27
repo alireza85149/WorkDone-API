fb65b5de699be17aac4f9e96943cf5327f8facc0
alireza <mohammadianalireza85149@gmail.com>
2026-07-27 13:31:10 +0330

    the submissions.models created.

--- apps/submissions/models.py
+++ apps/submissions/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from apps.contracts.models import Contract
 
-# Create your models here.
+class Submission(models.Model):
+
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        APPROVED = "approved", "Approved"
+        REVISION = "revision", "Needs Revision"
+
+    contract = models.ForeignKey(
+        Contract,
+        on_delete=models.CASCADE,
+        related_name="submissions"
+    )
+
+    message = models.TextField()
+
+    attachment = models.FileField(
+        upload_to="submissions/",
+        blank=True,
+        null=True
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+
+    submitted_at = models.DateTimeField(
+        auto_now_add=True
+    )
