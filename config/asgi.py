87186550098e7318bed5252a41fa98129811058b
alireza85149 <mohammadianalireza85149@gmail.com>
2026-08-16 16:11:34 +0330

    changing the asgi.py for debugging the websocket connection

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,17 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from apps.notifications.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 django_asgi_app = get_asgi_application()
+
+from apps.notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+
     "websocket": URLRouter(
         websocket_urlpatterns
     ),
